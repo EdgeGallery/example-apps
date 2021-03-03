1b8925d2451c087123c40d8dc6e2ab067a2f4150
Rama-Huawei <rama.subba.reddy.s@huawei.com>
2021-03-03 10:56:28 +0530

    Fixed the issues reported by pylint tool
    Signed-off-by: Rama-Huawei <rama.subba.reddy.s@huawei.com>

--- monitoring_service/backend/run.py
+++ monitoring_service/backend/run.py
@@ -13,14 +13,19 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
-
+"""
+    Starting the monitoring service server
+"""
 import logging
 from logging.handlers import RotatingFileHandler
 from monitoring.monitoring_service import start_server
 
 if __name__ == '__main__':
-    """Starting the monitoring service server"""
+    """
+       Starting the monitoring service server
+    """
     handler = RotatingFileHandler('/usr/app/log/monitoring_service.log',
                                   maxBytes=10000000, backupCount=10)
     handler.setLevel(logging.INFO)
     start_server(handler)
+
